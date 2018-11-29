e5c16f7d534310b9e5c65aff50ca0c1c773353ac
bash <rf6447.i.f@gmail.com>
2018-11-29 22:28:09 +0900

    3.2.2 検証メソッドのサンプルコードを追加

--- section03/3.2.1/SampleTest.rb
+++ section03/3.2.1/SampleTest.rb
@@ -4,4 +4,23 @@ class SampleTest < Minitest::Test
     def test_sample
         assert_equal 'RUBY', 'ruby'.upcase
     end
+
+    # equals a and b
+    def test_equal
+        a, b = 1, 1
+        assert_equal a, b
+    end
+
+    # Pass If a is true
+    def test_bool
+        a = true
+        assert a
+    end
+
+    # Pass If a is false
+    def test_refute
+        a = false
+        refute a
+    end 
+
 end
